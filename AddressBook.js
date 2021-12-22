5cb8df1920cdc118051654dd261840884c894e6e
aakashpotdar <potdaraakash@gmail.com>
2021-12-22 12:33:54 +0530

    function to retrive data and edit data is added in the code

--- AddressBook.js
+++ AddressBook.js
@@ -1,3 +1,5 @@
+let AddressBook=new Map();
+
 let FirstName='Aakash';
 let LastName='Potdar';
 let Address='MallikarjunNagar';
@@ -6,8 +8,17 @@ let Sate='MH';
 let Zip=47837;
 let PhoneNumber='+918003728743';
 let Email='Aakash@gmail.com';
+addData(FirstName,LastName,Address,City,Sate,PhoneNumber,Email,Zip);
 
-
+FirstName='shubham';
+LastName='Roddy';
+Address='Vijapur Road';
+City='Solapur';
+Sate='MH';
+Zip=47837;
+PhoneNumber='+916754894302';
+Email='shubs@gmail.com';
+addData(FirstName,LastName,Address,City,Sate,PhoneNumber,Email,Zip);
 
 function CheckNameValidation(name)
 {
@@ -67,3 +78,35 @@ function CheckPhoneNumberValidation(number)
 }
 console.log('cheking mobile number validation: '+CheckPhoneNumberValidation(PhoneNumber));
 console.log('-----------------------------------');
+
+function addData(firstName,lastName,address,city,sate,phoneNumber,email,zip){
+let Data=new Array();
+Data.push(firstName);
+Data.push(lastName);
+Data.push(address);
+Data.push(city);
+Data.push(sate);
+Data.push(phoneNumber);
+Data.push(email);
+Data.push(zip);
+AddressBook.set(FirstName,Data);
+}
+
+console.log(AddressBook)
+
+
+console.log('chking the data is available for name : Aakash '+AddressBook.has('Aakash'));
+
+AddressBook.get('Shubham');
+
+FirstName='shubham';
+LastName='Roddy';
+Address='Vijapur Road';
+City='Bengloru';
+Sate='KA';
+Zip=478342;
+PhoneNumber='+916754894364';
+Email='shubham@gmail.com';
+addData(FirstName,LastName,Address,City,Sate,PhoneNumber,Email,Zip);
+
+console.log(AddressBook)
